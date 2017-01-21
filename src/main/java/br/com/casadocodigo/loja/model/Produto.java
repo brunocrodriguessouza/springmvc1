84eb61818f3d5d4553cb7fc5f80d62928ab72599
brunoprogramadorjava <brunoprogramadorjava@gmail.com>
2017-01-21 21:47:07 -0200

    Cadastrando Precos dos produtos usando Enum, Relacionamento e o ModelAndView

--- src/main/java/br/com/casadocodigo/loja/model/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/model/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.model;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,9 +14,21 @@ public class Produto {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
 	private String titulo;
 	private String Descricao;
 	private int paginas;
+	
+	@ElementCollection
+	private List<Preco> precos;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getTitulo() {
 		return titulo;
@@ -38,7 +53,15 @@ public class Produto {
 	public void setPaginas(int paginas) {
 		this.paginas = paginas;
 	}
-	
+
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
+
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", Descricao=" + Descricao + ", paginas=" + paginas + "]";
--- src/main/webapp/WEB-INF/views/produtos/form.jsp
+++ src/main/webapp/WEB-INF/views/produtos/form.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,6 +23,13 @@
 			<input type="text" name="paginas">
 			<button type="submit">Cadastrar</button>
 		</div>
+		<c:forEach items="${tipos}" var="tipoPreco" varStatus="status">
+			<div>
+				<label>${tipoPreco} </label>
+				<input type="text" name="precos[${status.index}].valor">
+				<input type="hidden" name="precos[${status.index}].tipo" value="${tipoPreco}">
+			</div>
+		</c:forEach>
 	</form>
 </body>
 </html>
