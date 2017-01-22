d4ab5c18dc8b819e758c226b5ab1e69a40574dbd
brunoprogramadorjava <brunoprogramadorjava@gmail.com>
2017-01-22 01:27:02 -0200

    Listando os produtos usando o getResultList com os resultados da consulta do Banco de dados

--- src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
+++ src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -16,9 +18,13 @@ public class ProdutoDAO {
 	private EntityManager manager;
 
 	public void gravar(Produto produto) {
-		
+
 		manager.persist(produto);
 
+	}
+
+	public List<Produto> listar() {
+		return manager.createQuery("select p from Produto p", Produto.class).getResultList();
 	}
 
 }
