b6a0907e348263f59e7b482ab6b4c804e057c047
brunoprogramadorjava <brunoprogramadorjava@gmail.com>
2017-02-02 12:12:06 -0200

    Inserindo a data de lancamento do livro com formatacao de data usando um Default formatter

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -30,5 +33,15 @@ public class AppWebConfiguration {
 		rbMessageSource.setDefaultEncoding("UTF-8");
 		rbMessageSource.setCacheSeconds(1);
 		return rbMessageSource;
+	}
+	
+	@Bean
+	public FormattingConversionService mvcConversionService(){
+		FormattingConversionService fConversionService = new FormattingConversionService();
+		DateFormatterRegistrar registrar = new DateFormatterRegistrar();
+		registrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
+		registrar.registerFormatters(fConversionService);
+		
+		return fConversionService;
 	}
 }
